66717679ae46569a7d281d2cb198431e0d023e10
Yuki_NISHII <dubhemerakphecdamegrez@gmail.com>
2022-12-20 14:01:08 +0900

    add proto login

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import loginImage from './assets/login.png'
+import logoutImage from './assets/logout.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoggedIn, changeLogin] = useState(false)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='flex justify-center'>
+        {isLoggedIn ? (
+          <img src={loginImage} alt="login"/>
+        ) : (
+          <img src={logoutImage} alt="logout"/>
+        )}
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + TS + TailwindCSS</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => changeLogin((isLoggedIn) => !isLoggedIn)}>
+          {isLoggedIn ? 'Logout' : 'Login'}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
