16e1cc564705890b88543d5045fb23a4642c07d3
Yuki_NISHII <dubhemerakphecdamegrez@gmail.com>
2022-12-20 14:09:00 +0900

    install prettier & format code

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,25 @@ import logoutImage from './assets/logout.png'
 import './App.css'
 
 function App() {
-  const [isLoggedIn, changeLogin] = useState(false)
+    const [isLoggedIn, changeLogin] = useState(false)
 
-  return (
-    <div className="App">
-      <div className='flex justify-center'>
-        {isLoggedIn ? (
-          <img src={loginImage} alt="login"/>
-        ) : (
-          <img src={logoutImage} alt="logout"/>
-        )}
-      </div>
-      <h1>Vite + React + TS + TailwindCSS</h1>
-      <div className="card">
-        <button onClick={() => changeLogin((isLoggedIn) => !isLoggedIn)}>
-          {isLoggedIn ? 'Logout' : 'Login'}
-        </button>
-      </div>
-    </div>
-  )
+    return (
+        <div className="App">
+            <div className="flex justify-center">
+                {isLoggedIn ? (
+                    <img src={loginImage} alt="login" />
+                ) : (
+                    <img src={logoutImage} alt="logout" />
+                )}
+            </div>
+            <h1>Vite + React + TS + TailwindCSS</h1>
+            <div className="card">
+                <button onClick={() => changeLogin((isLoggedIn) => !isLoggedIn)}>
+                    {isLoggedIn ? 'Logout' : 'Login'}
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default App
