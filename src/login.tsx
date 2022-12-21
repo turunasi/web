9d570231416c06bc2548aaedfafc88c4aba05947
Yuki_NISHII <dubhemerakphecdamegrez@gmail.com>
2022-12-21 13:52:08 +0900

    format code

--- src/login.tsx
+++ src/login.tsx
@@ -1,11 +1,11 @@
-import "./constants";
+import './constants'
 import {
     COGNITO_IDENTITY_POOL_ID,
     COGNITO_REGION,
     USER_POOL_CLIENT_ID,
     USER_POOL_ID
-} from "./constants";
-import { Amplify, Auth } from 'aws-amplify';
+} from './constants'
+import { Amplify, Auth } from 'aws-amplify'
 // import { Authenticator } from '@aws-amplify/ui-react';
 // import '@aws-amplify/ui-react/styles.css';
 
@@ -16,40 +16,40 @@ Amplify.configure({
         identityPoolRegion: COGNITO_REGION,
         userPoolId: USER_POOL_ID,
         userPoolWebClientId: USER_POOL_CLIENT_ID
-    },
-});
+    }
+})
 
-
-const signUp = async(username:string, password:string) => {
+const signUp = async (username: string, password: string) => {
     try {
         const { user } = await Auth.signUp({
             username,
             password,
-            autoSignIn: { // optional - enables auto sign in after user is confirmed
-                enabled: true,
+            autoSignIn: {
+                // optional - enables auto sign in after user is confirmed
+                enabled: true
             }
-        });
-        console.log(user);
+        })
+        console.log(user)
     } catch (error) {
-        console.log('error signing up:', error);
+        console.log('error signing up:', error)
     }
 }
 
-const signIn = async() => {
+const signIn = async () => {
     try {
-        const user = await Auth.signIn('user', 'Dsuxjc85&');
-        console.log('signed in successfully', user);
+        const user = await Auth.signIn('user', 'Dsuxjc85&')
+        console.log('signed in successfully', user)
     } catch (error) {
-        console.log('error signing in', error);
+        console.log('error signing in', error)
     }
 }
 
-const signOut = async() => {
+const signOut = async () => {
     try {
-        await Auth.signOut();
+        await Auth.signOut()
     } catch (error) {
-        console.log('error signing in', error);
+        console.log('error signing in', error)
     }
 }
 
-export default signIn;
+export default signIn
